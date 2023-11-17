26eac2e52788f01af8bdf967604870b6d22523ed
teruroom <teruroom@gmail.com>
2023-11-18 01:09:50 +0900

    4th

--- src/FormApp.js
+++ src/FormApp.js
@@ -1,11 +1,18 @@
 import React,{Component} from "react"
 
 export class FormApp extends Component{
+  handleInput(){
+    console.log('handleInput called!')
+  }
+  send(){
+    console.log('send called!')
+  }
+
   render(){
     return(
       <div>
-        <input type="text" />
-        <button>SEND</button>
+        <input type="text" onChange={this.handleInput.bind(this)}/>
+        <button onClick={this.send.bind(this)}>SEND</button>
       </div>
     );
   }  
